47f80922e62963ee9705e544b1b5c45ebb535830
pha123661 <pha123661@gmail.com>
2021-11-26 22:21:51 +0800

    Add save weight

--- Final Project/Train_Test_Plot.py
+++ Final Project/Train_Test_Plot.py
@@ -1,14 +1,22 @@
+from pytorch_lightning import callbacks
 from Hyper_parameters import HyperParams
 from myDataLoader import get_loaders
 from Models import CNN_Model
 import numpy as np
 import pytorch_lightning as pl
-
+from pytorch_lightning.callbacks import ModelCheckpoint
 def main():
     # CNN
     CNN_model = CNN_Model()
-    trainer = pl.Trainer(gpus=1)
     train_loader, test_loader = get_loaders()
+    checkpoint_callback = ModelCheckpoint(
+        monitor='acc',
+        dirpath='./model_weights',
+        filename='CNN-{epoch:02d}-{acc:.2f}',
+        save_top_k=2,
+        mode='max',
+    )
+    trainer = pl.Trainer(gpus=1, callbacks=[checkpoint_callback])
     trainer.fit(CNN_model, train_loader, test_loader)
 
 
