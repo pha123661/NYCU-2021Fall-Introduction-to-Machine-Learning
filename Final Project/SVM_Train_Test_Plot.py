1e2c86aacb3aa20e58eda1ff323a19b16c7760e5
pha123661 <pha123661@gmail.com>
2021-11-27 15:04:44 +0800

    Add SVM with CNN

--- Final Project/SVM_Train_Test_Plot.py
+++ Final Project/SVM_Train_Test_Plot.py
@@ -4,11 +4,12 @@ from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score
 from myDataLoader import get_ndarrays
 import numpy as np
+from CNN_Train_Test_Plot import *
 
 def main():
-    x_train, y_train, x_test, y_test, x_valid, y_valid = get_ndarrays(test=True, flatten=True)
+    x_train, y_train, x_test, y_test, x_valid, y_valid = get_ndarrays()
     # mel: 128 x 128
-    SVM = SVC(kernel='linear')
+    SVM = SVC(kernel='poly')
     SVM.fit(x_train, y_train)
     y_pred = SVM.predict(x_test)
     print(confusion_matrix(y_test, y_pred))
