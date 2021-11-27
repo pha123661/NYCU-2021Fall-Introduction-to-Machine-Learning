4d2d7310b03471accdb131a28ca90b4454d2616f
pha123661 <pha123661@gmail.com>
2021-11-27 16:36:54 +0800

    Add grid search

--- Final Project/KNN_Train_Test_Plot.py
+++ Final Project/KNN_Train_Test_Plot.py
@@ -10,12 +10,20 @@ from CNN_Train_Test_Plot import *
 def main():
     x_train, y_train, x_test, y_test, x_valid, y_valid = get_ndarrays()
     # shape = (samples, 512*2*2)
-    KNNclf = KNeighborsClassifier(n_jobs=-1, n_neighbors=7) # get by grid search
+    for k in [5, 7, 10, 20, 40, 100]:
+        KNNclf = KNeighborsClassifier(n_jobs=-1, n_neighbors=k) # get by grid search
+        KNNclf.fit(x_train, y_train)
+        y_pred = KNNclf.predict(x_valid)
+        # print(confusion_matrix(y_test, y_pred))
+        print(f"k = {k}")
+        print(f"{accuracy_score(y_valid, y_pred):.6f} {recall_score(y_valid, y_pred, average='macro'):.6f} {precision_score(y_valid, y_pred, average='macro'):.6f}")
+        
+    KNNclf = KNeighborsClassifier(n_jobs=-1, n_neighbors=5) # get by grid search
     KNNclf.fit(x_train, y_train)
     y_pred = KNNclf.predict(x_test)
-    print(confusion_matrix(y_test, y_pred))
-    print(accuracy_score(y_test, y_pred), recall_score(y_test, y_pred, average='macro'), precision_score(y_test, y_pred, average='macro'))
-
+    # print(confusion_matrix(y_test, y_pred))
+    print(f"testing")
+    print(f"{accuracy_score(y_test, y_pred):.6f} {recall_score(y_test, y_pred, average='macro'):.6f} {precision_score(y_test, y_pred, average='macro'):.6f}")
         
 if __name__ == '__main__':
     main()
